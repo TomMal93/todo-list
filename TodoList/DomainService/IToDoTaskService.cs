e085de5f38b65d4b5d1812637e1a9514f164428a
Tomasz <Tomasz.Maleszyk93@gmail.com>
2020-08-05 13:18:26 +0200

    Services

---
+++ TodoList/DomainService/IToDoTaskService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Domain;
+using TodoList.Repository;
+
+namespace TodoList.DomainService
+{
+    public interface IToDoTaskService
+    {
+        Task<IEnumerable<ToDoTask>> GetAllToDoTasks(int page);
+        Task<ToDoTask> GetToDoTask(Guid toDoTaskID);
+        Task<ToDoTask> AddToDoTask(ToDoTask toDoTask);
+        Task<IEnumerable<ToDoTask>> GetToDoTasksByIsDoneStatus(int page, IsDoneFiltration isDoneFiltration);
+        Task<ToDoTask> UpdateToDoTask(Guid Id, ToDoTask toDoTask);
+        Task<bool> UpdateToDoTaskIsDone(Guid toDoTaskID, bool isDone);
+        Task<bool> UpdateToDoTaskPriorityStatus(Guid id, Priority priorityStatus);
+        Task<bool> DeleteToDoTask(Guid toDoTaskID);
+        Task<IEnumerable<ToDoTask>> GetToDoTasksByTitle(int page, string title);
+        Task<IEnumerable<ToDoTask>> GetToDoTasksByDescription(int page, string description);
+    }
+}
---
+++ TodoList/DomainService/ToDoTaskService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Domain;
+using TodoList.Repository;
+
+namespace TodoList.DomainService
+{
+    public class ToDoTaskService : IToDoTaskService
+    {
+        private readonly IToDoTaskRepository _toDoTaskRepository;
+
+        public ToDoTaskService(IToDoTaskRepository toDoTaskRepository)
+        {
+            _toDoTaskRepository = toDoTaskRepository;
+        }
+
+        public Task<ToDoTask> AddToDoTask(ToDoTask toDoTask)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteToDoTask(Guid toDoTaskID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ToDoTask>> GetAllToDoTasks(int page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ToDoTask> GetToDoTask(Guid toDoTaskID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ToDoTask>> GetToDoTasksByDescription(int page, string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ToDoTask>> GetToDoTasksByIsDoneStatus(int page, IsDoneFiltration isDoneFiltration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ToDoTask>> GetToDoTasksByTitle(int page, string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ToDoTask> UpdateToDoTask(Guid Id, ToDoTask toDoTask)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateToDoTaskIsDone(Guid toDoTaskID, bool isDone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateToDoTaskPriorityStatus(Guid id, Priority priorityStatus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
